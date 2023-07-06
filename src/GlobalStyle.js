eb0ee64fe86381f74feafd24d656f2a495d7fecf
Dorota Zielinska <dorcius@protonmail.com>
2023-07-06 20:10:23 +0200

    added Global Style, ThemeProvider, theme colors

---
+++ src/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+    box-sizing: border-box;
+}
+
+*, ::after,
+ ::before {
+  box-sizing: inherit;
+}
+
+body {
+    background-color: ${({ theme }) => theme.color.whisper};
+    font-family: 'Poppins', sans-serif;
+    word-break: break-word;
+}
+`;
---
+++ src/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+    color: {
+        white: "#FFFFFF",
+        whisper: "#F5F5FA",
+        waterloo: "#7E839A",
+        woodsmoke: "#18181B",
+        mystic: "#E4E6F0",
+        mineShaft: "#333333",
+        scienceBlue: "#0044CC",
+        stormGray: "#74788B",
+        silver: "#C4C4C4",
+        black: "#000000",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    }
+}
