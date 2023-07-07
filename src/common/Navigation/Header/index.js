8a6e9dbe06a5e0e13427b82f8311a2a80cd7aee2
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-07 13:54:41 +0200

    Removed empty lines

--- src/common/Navigation/Header/index.js
+++ src/common/Navigation/Header/index.js
@@ -1,13 +1,11 @@
 import { Wrapper } from "./styled";
 import { ReactComponent as CameraIcon } from "../Images/camera.svg";
 
-
-
 export const NavigationHeader = () => {
 
     return (
         <Wrapper>
-            <CameraIcon />
+           <CameraIcon />
             <h2>Movies Browser</h2>
         </Wrapper>
     );
