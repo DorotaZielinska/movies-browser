5fa62f27e71357c33b8cfef360c12dd7f44a025f
Dorota Zielinska <dorcius@protonmail.com>
2023-07-07 21:17:55 +0200

    added component TileList in common with styled

--- src/common/TileList/index.js
+++ src/common/TileList/index.js
@@ -1,11 +1,15 @@
+import { Genres } from "../Genres"
+import { Rating } from "../Rating"
 import { Content, Poster, StyledSubbTitle, StyledTitle, TileList } from "./styled"
+
 export const TileList = () => (
-<TileList>
-<Poster />
-<Content>
-   <StyledTitle />
-   <StyledSubbTitle />
-   
-</Content>
-</TileList>
+    <TileList>
+        <Poster />
+        <Content>
+            <StyledTitle />
+            <StyledSubbTitle />
+            <Genres />
+            <Rating />
+        </Content>
+    </TileList>
 )
--- src/common/TileList/styled.js
+++ src/common/TileList/styled.js
@@ -1,36 +1,35 @@
 import styled from "styled-components";
 
 export const TileList = styled.article`
-width: 324px;
-height: 650px;
-background-color: ${({ theme }) => theme.color.white};
-padding: 16px;
-border-radius: 5px;
-box-shadow: 0px 4px 12px 0px rgba(186, 199, 213, 0.50);
-display: grid;
-grid-template-rows: auto 1 fr;
-grid-template-columns: 1fr; 
+    width: 324px;
+    height: 650px;
+    background-color: ${({ theme }) => theme.color.white};
+    padding: 16px;
+    border-radius: 5px;
+    box-shadow: 0px 4px 12px 0px rgba(186, 199, 213, 0.50);
+    display: grid;
+    grid-template-rows: auto 1 fr;
+    grid-template-columns: 1fr; 
 `;
 
 export const Poster = styled.img`
-width: 292px;
-height: 434px;
+    width: 292px;
+    height: 434px;
 `;
 
 export const Content = styled.div`
-
 `;
 
 export const StyledTitle = styled.h1`
-font-weight: 500;
-font-size : 22px;
-color: ${({ theme }) => theme.color.woodsmoke};
-margin-bottom: 8px;
+    font-weight: 500;
+    font-size : 22px;
+    color: ${({ theme }) => theme.color.woodsmoke};
+    margin-bottom: 8px;
 `;
 
 export const StyledSubbTitle = styled.h2`
-font-weight: 400;
-font-size: 16px;
-color: ${({ theme }) => theme.color.waterloo};
+    font-weight: 400;
+    font-size: 16px;
+    color: ${({ theme }) => theme.color.waterloo};
 `;
 
