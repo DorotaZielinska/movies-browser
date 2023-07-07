e0388c7f9a532c915e5c943fc411ac2f308293bf
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-07 22:09:16 +0200

    Added styled-components and NavLink to navigation

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 
 export const NavigationContainer = styled.div`
     background: ${({ theme }) => theme.color.woodsmoke};
@@ -11,5 +12,39 @@ export const NavigationContainer = styled.div`
 export const NavigationWrapper = styled.nav`
     display:flex;
     align-items: center;
+    flex-shrink: 0;
 `;
 
+export const List = styled.ul`
+    display: flex;
+    justify-content: center;
+    list-style-type: none;
+    gap: 16px;
+    flex-shrink: 0;
+    padding-left: 80px;
+`;
+
+export const Item = styled.li`
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    text-transform: uppercase;
+    `;
+
+export const StyledNavLink = styled(NavLink)`
+    text-decoration-line: none;
+    color: ${({ theme }) => theme.color.white};
+    display: inline-flex;
+    height: 48px;
+    padding: 8px 24px;
+    align-items: center;
+    gap: 10px;
+    flex-shrink: 0;
+    border-radius: 24px;
+    border: 1px solid transparent;
+
+    &.active {
+         border: 1px solid ${({ theme }) => theme.color.white};
+    }
+`;
