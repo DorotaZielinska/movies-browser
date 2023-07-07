af2dcf9df4ba63805754d18859942f98f7158b57
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-07 12:39:32 +0200

    Added Navigation Header component with styled.js

---
+++ src/common/Navigation/Header/index.js
@@ -0,0 +1,14 @@
+import { Wrapper } from "./styled";
+import { ReactComponent as CameraIcon } from "../Images/camera.svg";
+
+
+
+export const NavigationHeader = () => {
+
+    return (
+        <Wrapper>
+            <CameraIcon />
+            <h2>Movies Browser</h2>
+        </Wrapper>
+    );
+};
---
+++ src/common/Navigation/Header/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    flex-wrap: nowrap;
+    gap: 16px;
+`;
