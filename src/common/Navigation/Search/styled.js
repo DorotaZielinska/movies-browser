e3724433ad4c9b4e4019a0b0e8e4a6751b0176d7
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-08 13:43:19 +0200

    Removed  flex-shrink

--- src/common/Navigation/Search/styled.js
+++ src/common/Navigation/Search/styled.js
@@ -3,14 +3,12 @@ import styled from "styled-components";
 import searchIcon from "../Images/search.svg";
 
 export const Wrapper = styled.div`
-    flex-shrink: 0;
     padding-left: 387px;
 `;
 
 export const Input = styled.input`
     width: 432px;
     height: 48px;
-    flex-shrink: 0;
     border-radius: 33px;
     border: 1px solid ${({ theme }) => theme.color.mystic};
     background: ${({ theme }) => theme.color.white};
@@ -19,5 +17,4 @@ export const Input = styled.input`
     background-repeat: no-repeat;
     background-position: 24px center;
     background-size: 24px 24px;
-    padding-left: 64px;
 `;
