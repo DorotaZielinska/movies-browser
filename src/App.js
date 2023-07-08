6fcf1d26ff335e5816b59a2c127fedefb1a20025
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-08 23:36:14 +0200

    Added Switch to app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import { HashRouter } from "react-router-dom"
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom"
 
 import { Navigation } from "./common/Navigation";
 import { TileList } from "./common/TileList";
+import Loading from "./features/AsideActions/Actions/loading"
 
 function App() {
 
   return (
     < >
       <HashRouter>
-        <Navigation toMovies={"/movies"} toPeople={"/people"} />
-      </HashRouter>
-      <TileList />
+          <Navigation toMovies={"/movies"} toPeople={"/people"} />
+          <Switch>
+            <Route path="/people">
+              <Loading />
+            </Route>
+            <Route path="/movies">
+              <TileList />
+            </Route>
+            <Route patch="/">
+              <Redirect to="/movies" />
+            </Route>
+          </Switch>
+        </HashRouter>
     </>
   );
 }
