310ff39c1457e063f560828f56735b77edae4bdf
Dorota Zielinska <dorcius@protonmail.com>
2023-07-08 13:58:01 +0200

    Added component features MovieList

---
+++ src/features/MovieList/index.js
@@ -0,0 +1,11 @@
+import { ItemMoviesList, MovieListContainer, MovieListTitle } from "./styled";
+
+export const MovieList = () => (
+    <MovieListContainer>
+        <MovieListTitle></MovieListTitle>
+        <MovieList>
+            <ItemMoviesList></ItemMoviesList>
+        </MovieList>
+
+    </MovieListContainer>
+)
