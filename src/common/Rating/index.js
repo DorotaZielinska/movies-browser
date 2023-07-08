c89a736e33fc1dc6035c8153273546f9ac83bc72
Dorota Zielinska <dorcius@protonmail.com>
2023-07-08 13:39:33 +0200

    changed RatingIcon to reactComponent

--- src/common/Rating/index.js
+++ src/common/Rating/index.js
@@ -1,9 +1,9 @@
 import { RatingContainter, RatingIcon, Vote, Votes } from "./styled";
-import shape from "./shape-star.svg";
+import { ReactComponent as RatingIcon } from "./shape-star.svg";
 
 export const Rating = () => (
     <RatingContainter>
-        <RatingIcon src= {shape} alt="star" />
+        <RatingIcon />
         <Vote>7,8</Vote>
         <Votes>35 votes</Votes>
     </RatingContainter>
