4b00f6b7185fff0831593a133b51ec925563f657
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-09 13:24:46 +0200

    a set of addresses to the api with the key

---
+++ src/features/apiUrlData.js
@@ -0,0 +1,13 @@
+export const key = "9ecbadad1fc39beb07bafb3db1e72d97";
+export const searchAPIUrl = "https://api.themoviedb.org/3/search";
+export const popularMoviesUrl = "https://api.themoviedb.org/3/movie/popular";
+export const popularPeopleUrl = "https://api.themoviedb.org/3/person/popular";
+export const imagesFullApi = "https://image.tmdb.org/t/p";
+export const movieIdAPI = "https://api.themoviedb.org/3/movie/";
+export const personAPI = "https://api.themoviedb.org/3/person/";
+export const imagesAPI200 = "https://image.tmdb.org/t/p/w200/";
+export const imagesAPI300and450 =
+  "https://image.tmdb.org/t/p/w300_and_h450_bestv2/";
+export const imagesAPIw400 = "https://image.tmdb.org/t/p/w400/";
+export const imagesAPI600and900 =
+  "https://image.tmdb.org/t/p/w600_and_h900_bestv2/";
