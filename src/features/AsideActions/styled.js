4eeb878a167249cd7d03889f03318159b1f1d21b
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-09 15:02:52 +0200

    add styles for warning component

--- src/features/AsideActions/styled.js
+++ src/features/AsideActions/styled.js
@@ -22,4 +22,40 @@ export const Image = styled(image)`
   align-self: center;
 `;
 
-export const Warning = styled(warningIcon)``;
+export const Warning = styled(warningIcon)`
+  display: block;
+  margin: 0 auto;
+`;
+
+export const Title = styled.h2`
+  font-size: 36px;
+  color: ${({ theme }) => theme.color.woodsmoke};
+  font-weight: 600;
+  line-height: 120%;
+  margin: 0;
+`;
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  border: none;
+  background-color: ${({ theme }) => theme.color.scienceBlue};
+  padding: 16px 24px;
+  border-radius: 5px;
+`;
+
+export const Content = styled.p`
+  font-size: 14px;
+  color: ${({ theme }) => theme.color.white};
+  font-weight: 700;
+  line-height: 19px;
+  margin: 0;
+`;
+export const Container = styled.div`
+  display: flex;
+  gap: 24px;
+  margin-top: 24px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
