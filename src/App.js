d6324e2bd807fccded60861d95a3f8fa0e1cd07b
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-10 12:27:40 +0200

    Clean up

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import { Navigation } from "./common/Navigation";
 import MovieList from "./features/MovieList";
 
 function App() {
-  
+
   return (
     <>
       <HashRouter>
         <Navigation toMovies={"/movies"} toPeople={"/people"} />
-                 <Switch>
-            <Route path="/people">
-            </Route>
-            <Route path="/movies">
-               <MovieList />
-            </Route>
-            <Route patch="/">
-              <Redirect to="/movies" />
-            </Route>
-          </Switch>
+        <Switch>
+          <Route path="/people">
+          </Route>
+          <Route path="/movies">
+            <MovieList />
+          </Route>
+          <Route patch="/">
+            <Redirect to="/movies" />
+          </Route>
+        </Switch>
 
       </HashRouter>
     </>
