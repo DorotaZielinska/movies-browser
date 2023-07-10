4160898c83429ee3472ffcb6b8e83862c41c81b2
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-10 16:25:10 +0200

    add action dispatch in case of error

--- src/features/movieListSaga.js
+++ src/features/movieListSaga.js
@@ -1,20 +1,24 @@
-import { call, put, select } from "redux-saga/effects";
-import { fetchMoviesList, selectPage } from "./movieListSlice";
+import { call, put, select, delay } from "redux-saga/effects";
+import {
+  fetchMoviesList,
+  fetchMoviesListError,
+  selectPage,
+} from "./movieListSlice";
 import { getGenres, getPopularMovies } from "./getDataApi";
 
-
 function* fetchMoviesListHandler() {
-    try {
-        const page = yield select(selectPage);
-        const genres = yield call(getGenres);
-        let data;
-        data = yield call(getPopularMovies, { page: page});
-        yield put(fetchMoviesList({ data, genres}));
-    } catch(error) {
-        yield put()
-    }
+  try {
+    yield delay(2000);
+    const page = yield select(selectPage);
+    const genres = yield call(getGenres);
+    let data;
+    data = yield call(getPopularMovies, { page: page });
+    yield put(fetchMoviesList({ data, genres }));
+  } catch (error) {
+    yield put(fetchMoviesListError());
+  }
 }
 
 export function* watchFetchMoviesList() {
-    yield fetchMoviesListHandler();
+  yield fetchMoviesListHandler();
 }
