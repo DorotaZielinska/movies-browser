bdd4f3a7c345ce910c1f54d60c6a2f85877e2dfa
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-10 20:38:07 +0200

    Moved placeholder logical from Nav. to Search

--- src/common/Navigation/Search/index.js
+++ src/common/Navigation/Search/index.js
@@ -1,11 +1,34 @@
-import { Wrapper, Input } from "./styled";
+import { useRouteMatch } from "react-router-dom";
 
-export const Search = ({ placeholder }) => {
+import { Wrapper, Input } from "./styled";
+import { searchQueryParamName } from "./searchQueryParamName";
+import { useQueryParameter, useReplaceQueryParameter } from "./queryParameter";
+
+export const Search = () => {
+    const moviesMatch = useRouteMatch("/movies");
+    const peopleMatch = useRouteMatch("/people");
+    const placeholder = moviesMatch ? "Search for movies..." : "Search for people...";
+
+    const query = useQueryParameter(searchQueryParamName);
+    const replaceQueryParameter = useReplaceQueryParameter();
+
+    const onInputChange = ({ target }) => {
+        replaceQueryParameter(
+            searchQueryParamName,
+            target.value.trim() !== "" ? target.value : undefined,
+            peopleMatch ? 'people' : undefined,
+            moviesMatch ? 'movies' : undefined,
+        );
+    };
 
     return (
         <>
             <Wrapper>
-                <Input placeholder={placeholder} />
+                <Input
+                    placeholder={placeholder}
+                    value={query || ""}
+                    onChange={onInputChange}
+                />
             </Wrapper>
         </>
     );
--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,20 +1,9 @@
-import { useState } from "react";
-
 import { NavigationContainer, NavigationWrapper, List, Item, StyledNavLink } from "./styled";
 import { NavigationLogo } from "./Logo";
 import { Container } from "../Container";
 import { Search } from "./Search";
 
 export const Navigation = ({ toMovies, toPeople }) => {
-
-    const movies = "Search for movies...";
-    const people = "Search for people...";
-
-    const [placeholder, setPlaceholder] = useState(movies);
-
-    const handleNavLinkClick = (placeholder) => {
-        setPlaceholder(placeholder);
-    };
 
     return (
         <NavigationContainer>
@@ -23,13 +12,13 @@ export const Navigation = ({ toMovies, toPeople }) => {
                     <NavigationLogo />
                     <List>
                         <Item>
-                            <StyledNavLink to={toMovies} onClick={() => handleNavLinkClick(movies)}>Movies</StyledNavLink>
+                            <StyledNavLink to={toMovies}>Movies</StyledNavLink>
                         </Item>
                         <Item>
-                            <StyledNavLink to={toPeople} onClick={() => handleNavLinkClick(people)}>People</StyledNavLink>
+                            <StyledNavLink to={toPeople}>People</StyledNavLink>
                         </Item>
                     </List>
-                    <Search placeholder={placeholder} />
+                    <Search />
                 </NavigationWrapper>
             </Container>
         </NavigationContainer>
