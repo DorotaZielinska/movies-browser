2919aa22939f6c3968d277b351d54a7fc896fc24
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-10 12:23:36 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom"
 import { Navigation } from "./common/Navigation";
-
 import MovieList from "./features/MovieList";
 
-
-
-
-import { TileList } from "./common/TileList";
-import NotFound from "./features/AsideActions/NotFound/notFound";
-
-
 function App() {
+  
   return (
     <>
       <HashRouter>
         <Navigation toMovies={"/movies"} toPeople={"/people"} />
-
-      <MovieList>
-        <Switch>
-          <Route path="/people">
-          </Route>
-          <Route path="/movies">
-            <TileList />
-          </Route>
-          <Route patch="/">
-            <Redirect to="/movies" />
-          </Route>
-        </Switch>
+                 <Switch>
+            <Route path="/people">
+            </Route>
+            <Route path="/movies">
+               <MovieList />
+            </Route>
+            <Route patch="/">
+              <Redirect to="/movies" />
+            </Route>
+          </Switch>
 
       </HashRouter>
     </>
