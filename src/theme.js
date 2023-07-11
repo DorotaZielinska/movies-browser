43b4896d89f74a5a18224c9e1d9a5bbe99e40fb6
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-11 10:11:03 +0200

    add new color for svg in pagination

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,21 @@
 export const theme = {
-    color: {
-        white: "#FFFFFF",
-        whisper: "#F5F5FA",
-        waterloo: "#7E839A",
-        woodsmoke: "#18181B",
-        mystic: "#E4E6F0",
-        mineShaft: "#333333",
-        scienceBlue: "#0044CC",
-        stormGray: "#74788B",
-        silver: "#C4C4C4",
-        black: "#000000",
-    },
-    breakpoint: {
-        desktop: "1200px",
-        ipod: "1024px",
-        small: "767px",
-        mobile: "320px",
-    }
-}
+  color: {
+    white: "#FFFFFF",
+    whisper: "#F5F5FA",
+    waterloo: "#7E839A",
+    woodsmoke: "#18181B",
+    mystic: "#E4E6F0",
+    mineShaft: "#333333",
+    pattensBlue: "#D6E4FF",
+    scienceBlue: "#0044CC",
+    stormGray: "#74788B",
+    silver: "#C4C4C4",
+    black: "#000000",
+  },
+  breakpoint: {
+    desktop: "1200px",
+    ipod: "1024px",
+    small: "767px",
+    mobile: "320px",
+  },
+};
