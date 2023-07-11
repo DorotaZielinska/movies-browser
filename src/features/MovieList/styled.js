dae7e721b542ca420acc46be92a4b5575c29995a
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-07-11 18:44:31 +0200

    added @media for small Movielist TileList

--- src/features/MovieList/styled.js
+++ src/features/MovieList/styled.js
@@ -3,7 +3,11 @@ import styled from "styled-components";
 export const MovieListTitle = styled.h1`
     font-size: 36px;
     font-weight: 600;
-    color: ${({ theme }) => theme.color.woodsmoke}; 
+    color: ${({ theme }) => theme.color.woodsmoke};
+    
+    @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+        margin-left: 16px;
+    }
 `;
 
 export const MoviesList = styled.ul` 
@@ -12,6 +16,10 @@ export const MoviesList = styled.ul`
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-gap: 24px;
+    
+    @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+        grid-template-columns: 1fr;
+    }
 `;
 
 export const ItemMoviesList = styled.li` 
