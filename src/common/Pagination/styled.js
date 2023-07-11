8ce28a1ceef1c6b7beb26aaf0280573129c318c6
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-11 10:11:56 +0200

    add styled components for pagination

---
+++ src/common/Pagination/styled.js
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+import { ReactComponent as backIcon } from "./icons/backIcon.svg";
+import { ReactComponent as nextIcon } from "./icons/nextIcon.svg";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 24px 0 100px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  gap: 12px;
+`;
+export const Button = styled.button`
+  border: none;
+  border-radius: 5px;
+  padding: 8px 16px;
+  background-color: ${({ theme }) => theme.color.pattensBlue};
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.color.mystic};
+    color: ${({ theme }) => theme.color.woodsmoke};
+  }
+`;
+
+export const ButtonText = styled.p`
+  margin: 0;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.4;
+`;
+
+export const BackIcon = styled(backIcon)`
+  color: ${({ theme }) => theme.color.scienceBlue};
+  margin-right: 8px;
+
+  color: ${({ disabled, theme }) =>
+    disabled ? theme.color.waterloo : theme.color.scienceBlue};
+`;
+export const NextIcon = styled(nextIcon)`
+  margin-left: 8px;
+  color: ${({ disabled, theme }) =>
+    disabled ? theme.color.waterloo : theme.color.scienceBlue};
+`;
+
+export const Counter = styled.p`
+  display: flex;
+  gap: 8px;
+  margin: 0 24px;
+  color: ${({ theme }) => theme.color.waterloo};
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 1.5;
+`;
+
+export const Page = styled.span`
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 1.5;
+  color: ${({ theme }) => theme.color.woodsmoke};
+`;
