02b2713c439e3e8afaebc32b18682dda84738f28
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-12 17:18:44 +0200

    Added dispatch to target.value

--- src/common/Navigation/Search/index.js
+++ src/common/Navigation/Search/index.js
@@ -1,13 +1,16 @@
 import { useRouteMatch } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 import { Wrapper, Input } from "./styled";
 import { searchQueryParamName } from "./searchQueryParamName";
 import { useQueryParameter, useReplaceQueryParameter } from "./queryParameter";
+import { setSearchQuery } from "../../../features/movieListSlice"
 
 export const Search = () => {
     const moviesMatch = useRouteMatch("/movies");
     const peopleMatch = useRouteMatch("/people");
     const placeholder = moviesMatch ? "Search for movies..." : "Search for people...";
+    const dispatch = useDispatch();
 
     const query = useQueryParameter(searchQueryParamName);
     const replaceQueryParameter = useReplaceQueryParameter();
@@ -19,7 +22,10 @@ export const Search = () => {
             peopleMatch ? 'people' : undefined,
             moviesMatch ? 'movies' : undefined,
         );
+        console.log("Testing dispatch value ", target.value);
+        dispatch(setSearchQuery(target.value))
     };
+
 
     return (
         <>
