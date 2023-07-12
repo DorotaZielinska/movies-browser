19a1e75ab5d9ccf9928ad9f922d514a5af395d35
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-12 16:06:04 +0200

    add new actions , fixed selector path

--- src/features/movieListSlice.js
+++ src/features/movieListSlice.js
@@ -3,10 +3,10 @@ import { createSlice } from "@reduxjs/toolkit";
 const moviesListSlice = createSlice({
   name: "moviesList",
   initialState: {
-    page: 1,
     movies: [],
     genres: [],
     status: "loading",
+    page: 1,
   },
   reducers: {
     fetchMoviesList: (state, { payload: movies }) => {
@@ -19,15 +19,34 @@ const moviesListSlice = createSlice({
     fetchMoviesListError: (state) => {
       state.status = "error";
     },
+    nextPage: (state, { payload: currentPage }) => {
+      state.page = currentPage + 1;
+    },
+    lastPage: (state, { payload: currentPage }) => {
+      state.page = currentPage = 500;
+    },
+    previousPage: (state, { payload: currentPage }) => {
+      state.page = currentPage - 1;
+    },
+    firstPage: (state, { payload: currentPage }) => {
+      state.page = currentPage = 1;
+    },
   },
 });
-export const { fetchMoviesList, fetchMoviesListError } =
-  moviesListSlice.actions;
+
+export const {
+  fetchMoviesList,
+  fetchMoviesListError,
+  nextPage,
+  lastPage,
+  previousPage,
+  firstPage,
+} = moviesListSlice.actions;
 
 export const selectMoviesListState = (state) => state.moviesList;
 export const selectMoviesList = (state) => selectMoviesListState(state).movies;
 export const selectGenres = (state) => selectMoviesListState(state).genres;
 export const selectStatus = (state) => selectMoviesListState(state).status;
-export const selectPage = (state) => selectMoviesList(state).page;
+export const selectPage = (state) => selectMoviesListState(state).page;
 
 export default moviesListSlice.reducer;
