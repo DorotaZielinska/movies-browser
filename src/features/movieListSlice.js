75905e82fb8c17a0724ffd6002eddce13ed6e6ef
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-12 17:17:01 +0200

    Added query to slice

--- src/features/movieListSlice.js
+++ src/features/movieListSlice.js
@@ -7,6 +7,7 @@ const moviesListSlice = createSlice({
     movies: [],
     genres: [],
     status: "loading",
+    query: "",
   },
   reducers: {
     fetchMoviesList: (state, { payload: movies }) => {
@@ -19,9 +20,13 @@ const moviesListSlice = createSlice({
     fetchMoviesListError: (state) => {
       state.status = "error";
     },
+    setSearchQuery:(state, {payload: newQuery}) => {
+      console.log("newQuery ", newQuery);
+      state.query = newQuery;
+    }
   },
 });
-export const { fetchMoviesList, fetchMoviesListError } =
+export const { fetchMoviesList, fetchMoviesListError, setSearchQuery } =
   moviesListSlice.actions;
 
 export const selectMoviesListState = (state) => state.moviesList;
@@ -29,5 +34,6 @@ export const selectMoviesList = (state) => selectMoviesListState(state).movies;
 export const selectGenres = (state) => selectMoviesListState(state).genres;
 export const selectStatus = (state) => selectMoviesListState(state).status;
 export const selectPage = (state) => selectMoviesList(state).page;
+export const selectQuery = (state) => selectMoviesList(state).query;
 
 export default moviesListSlice.reducer;
