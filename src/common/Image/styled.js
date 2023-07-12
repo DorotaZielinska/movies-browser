163aafe77a2e017cf3923aad4ac0a7d889f95258
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-07-12 14:40:37 +0200

    adde Image component for noMoviePoster

---
+++ src/common/Image/styled.js
@@ -0,0 +1,38 @@
+import styled, { css} from "styled-components";
+import camera from "../Navigation/Images/camera.svg"
+
+
+export const ImageWrapper = styled.div`
+    background-color: ${({ theme }) => theme.color.silver};
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 292px;
+    height: 434px;
+`;
+
+export const Poster = styled.div.attrs(
+    ({ background }) =>
+    background && {
+        style: {
+            backgroundImage: `url(${background})`,
+        },
+    }
+)`
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        border-radius: 5px;
+        width: 100%;
+        height: 100%;
+
+        ${({ noMoviePoster }) => 
+            noMoviePoster && 
+            css`
+            background-image: url(${camera});
+            width: 100px;
+            height: 100px;
+                `
+        }
+`;
