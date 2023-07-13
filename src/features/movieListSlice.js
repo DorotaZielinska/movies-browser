c950502f413dfbfdf9fb07c37c5318dafd272bdd
MirekSobota <miroslaw.sobota@gmail.com>
2023-07-13 22:21:51 +0200

    Cleaned up code and add reducers

--- src/features/movieListSlice.js
+++ src/features/movieListSlice.js
@@ -10,30 +10,31 @@ const moviesListSlice = createSlice({
     query: "",
   },
   reducers: {
-    fetchMoviesList: (state, { payload: movies }) => {
+    fetchMoviesListLoad: (state) => {
       state.status = "loading";
+    },
+    fetchMoviesListSuccess: (state, { payload: movies }) => {
       state.movies = movies.data.results;
       state.genres = movies.genres;
       state.status = "success";
     },
-
     fetchMoviesListError: (state) => {
       state.status = "error";
     },
-    setSearchQuery:(state, {payload: newQuery}) => {
-      console.log("newQuery ", newQuery);
+    setSearchQuery: (state, { payload: newQuery }) => {
       state.query = newQuery;
-    }
+    },
+
   },
 });
-export const { fetchMoviesList, fetchMoviesListError, setSearchQuery } =
+export const { fetchMoviesListLoad, fetchMoviesListSuccess, fetchMoviesListError, setSearchQuery } =
   moviesListSlice.actions;
 
 export const selectMoviesListState = (state) => state.moviesList;
 export const selectMoviesList = (state) => selectMoviesListState(state).movies;
 export const selectGenres = (state) => selectMoviesListState(state).genres;
 export const selectStatus = (state) => selectMoviesListState(state).status;
-export const selectPage = (state) => selectMoviesList(state).page;
-export const selectQuery = (state) => selectMoviesList(state).query;
+export const selectPage = (state) => selectMoviesListState(state).page;
+export const selectQuery = (state) => selectMoviesListState(state).query;
 
 export default moviesListSlice.reducer;
