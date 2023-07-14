38c21cc42cb2b0545787962641a1829061a114ce
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-14 17:36:52 +0200

    one worker has been removed

--- src/features/movieListSaga.js
+++ src/features/movieListSaga.js
@@ -1,36 +1,23 @@
 import { call, put, select, takeLatest } from "redux-saga/effects";
 import {
+  fetchMoviesListLoad,
   fetchMoviesListSuccess,
-  fetchMoviesList,
   fetchMoviesListError,
   selectPage,
-  changePage,
 } from "./movieListSlice";
 import { getGenres, getPopularMovies } from "./getDataApi";
-
-function* fetchChangePageHandler() {
-  try {
-    const page = yield select(selectPage);
-    const genres = yield call(getGenres);
-    const data = yield call(getPopularMovies, page);
-    yield put(fetchMoviesList({ data, genres }));
-  } catch (error) {
-    yield put(fetchMoviesListError());
-  }
-}
 
 function* getPopularMoviesHandler() {
   try {
     const page = yield select(selectPage);
     const genres = yield call(getGenres);
     const data = yield call(getPopularMovies, page);
-    yield put(fetchMoviesList({ data, genres }));
+    yield put(fetchMoviesListSuccess({ data, genres }));
   } catch (error) {
     yield put(fetchMoviesListError());
   }
 }
 
 export function* watchFetchMoviesList() {
-  yield takeLatest(changePage.type, fetchChangePageHandler);
-  yield (fetchMoviesListSuccess.type, getPopularMoviesHandler());
+  yield takeLatest(fetchMoviesListLoad.type, getPopularMoviesHandler);
 }
