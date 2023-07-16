e205219ee51753dc5fd62a4423639e132ccbd00f
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-07-16 21:19:03 +0200

    deleted mediaquery from main Contener, added auto

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -2,8 +2,5 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   max-width: 1368px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-    margin: auto 16px;
-  }
+  margin: auto;
 `;
