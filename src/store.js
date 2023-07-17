274c706fe0855c1573f2e6b3b48b604173fd9dac
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-17 16:27:11 +0200

    changed path for movieDetailsReducer

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./rootSaga";
-import movieDetailsReducer from "./features/movieDetailsSlice";
+import movieDetailsReducer from "./features/MovieDetails/movieDetailsSlice";
 import moviesListReducer from "./features/movieListSlice";
 
 const sagaMiddleware = createSagaMiddleware();
