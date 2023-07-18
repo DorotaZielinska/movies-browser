de8cfcda9d7d64fee90ff9d037b974ab767f459e
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-18 13:25:33 +0200

    add path to movies:id for MovieDetails

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { HashRouter, Switch, Route, Redirect } from "react-router-dom"
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import { Navigation } from "./common/Navigation";
 import MovieList from "./features/MovieList";
+import { MovieDetails } from "./features/MovieDetails/index";
 
 function App() {
-
   return (
     <>
       <HashRouter>
         <Navigation toMovies={"/movies"} toPeople={"/people"} />
         <Switch>
-          <Route path="/people">
+          <Route path="/movies/:id">
+            <MovieDetails />
           </Route>
+          <Route path="/people"></Route>
           <Route path="/movies">
             <MovieList />
           </Route>
@@ -18,7 +20,6 @@ function App() {
             <Redirect to="/movies" />
           </Route>
         </Switch>
-
       </HashRouter>
     </>
   );
