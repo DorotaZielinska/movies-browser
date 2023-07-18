e71ad46230ff34e69ef2828fb0998486e3763cc3
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-18 13:37:25 +0200

    changed item  to link

--- src/common/TileList/index.js
+++ src/common/TileList/index.js
@@ -1,29 +1,34 @@
-
-import { Genres } from "../Genres"
+import { Genres } from "../Genres";
 import { MovieImage } from "../Image";
-import { Rating } from "../Rating"
-import { Content, MovieTileList, StyledSubTitle, StyledTitle } from "./styled"
- 
-export const TileList = ({ title, year, genres, vote, votes, poster }) => {
-    const getYear = (string) => {
-        return string.split("-")[0];
-    }
+import { Rating } from "../Rating";
+import {
+  Content,
+  MovieTileList,
+  StyledSubTitle,
+  StyledTitle,
+  GetDetailsLink,
+} from "./styled";
 
-    return(
-        (
-            <MovieTileList>
-                <MovieImage poster={poster}/>
-                <Content>
-                    <div>
-                        <StyledTitle>{title}</StyledTitle>
-                        {year && <StyledSubTitle>{getYear(year)}</StyledSubTitle>}
-                        <Genres genres={genres} />
-                    </div>
-                    <div>
-                        <Rating vote={vote} votes={votes} />
-                    </div>
-                </Content>
-            </MovieTileList>
-        )
-    )
-}
+export const TileList = ({ title, year, genres, vote, votes, poster, id }) => {
+  const getYear = (string) => {
+    return string.split("-")[0];
+  };
+
+  return (
+    <GetDetailsLink to={`/movies/${id}`} key={id}>
+      <MovieTileList>
+        <MovieImage poster={poster} />
+        <Content>
+          <div>
+            <StyledTitle>{title}</StyledTitle>
+            {year && <StyledSubTitle>{getYear(year)}</StyledSubTitle>}
+            <Genres genres={genres} />
+          </div>
+          <div>
+            <Rating vote={vote} votes={votes} />
+          </div>
+        </Content>
+      </MovieTileList>
+    </GetDetailsLink>
+  );
+};
