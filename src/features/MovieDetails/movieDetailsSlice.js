d4f779dbce5a39c5a5f9be8aaf87b4c036e3bd4f
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-19 10:28:45 +0200

    add new actions, updated initialState

--- src/features/MovieDetails/movieDetailsSlice.js
+++ src/features/MovieDetails/movieDetailsSlice.js
@@ -3,24 +3,45 @@ import { createSlice } from "@reduxjs/toolkit";
 const movieDetailsSlice = createSlice({
   name: "movieDetails",
   initialState: {
-    movieId: "",
-    details: [],
-    credits: [],
+    movieId: null,
+    details: null,
+    credits: null,
     status: "loading",
   },
   reducers: {
+    fetchMovieDetailsSuccess: (state) => {
+      state.status = "success";
+    },
+
     fetchMovieDetails: (state, { payload: details, credits }) => {
       state.details = details;
       state.credits = credits;
     },
+
     getMovieId: (state, { payload: id }) => {
-      state.status = "loading";
       state.movieId = id;
+    },
+
+    fetchMoviesListError: (state) => {
+      state.status = "error";
+    },
+
+    resetMovieDetails: (state) => {
+      state.movieId = null;
+      state.details = null;
+      state.credits = null;
+      state.status = "loading";
     },
   },
 });
 
-export const { fetchMovieDetails, getMovieId } = movieDetailsSlice.actions;
+export const {
+  fetchMovieDetails,
+  getMovieId,
+  fetchMovieDetailsSuccess,
+  fetchMoviesListError,
+  resetMovieDetails,
+} = movieDetailsSlice.actions;
 
 export const selectMovieState = (state) => state.movieDetails;
 export const selectMovieId = (state) => selectMovieState(state).movieId;
