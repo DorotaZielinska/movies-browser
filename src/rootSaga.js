cb6e455c7f6e5fcdd2e110addd08850b552e0e2a
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-07-20 15:04:24 +0200

    added watchFetchPeopleDetails in root Saga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -2,11 +2,13 @@ import { all } from "redux-saga/effects";
 import { watchFetchMoviesList } from "./features/MovieList/movieListSaga";
 import { watchFetchMovieDetails } from "./features/movieDetailsSaga";
 import { watchFetchPeopleList } from "./features/PeopleList/peopleSaga";
+import { watchFetchPeopleDetails } from "./features/PeopleDetails/peopleDetailsSaga";
 
 export default function* rootSaga() {
     yield all([
         watchFetchMoviesList(),
         watchFetchMovieDetails(),
         watchFetchPeopleList(),
+        watchFetchPeopleDetails(),
     ]);
 }
