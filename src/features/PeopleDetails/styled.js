9cca0dcb82479d85304337deb624cb7308052cfe
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-07-20 15:03:26 +0200

    added styled.js in PeopleDetails

---
+++ src/features/PeopleDetails/styled.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+export const PeopleDetailsTitle = styled.h1`
+  margin-top: 64px;
+  margin-bottom: 24px;
+  font-size: 36px;
+  font-weight: 600;
+  line-height: 1.2;
+  color: ${({ theme }) => theme.color.woodsmoke};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.desktopMax}) {
+    margin: 16px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.ipod}) {
+    margin: 16px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
+    margin: 16px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}) {
+    margin-left: 16px;
+    margin-top: 24px;
+    margin-bottom: 12px;
+    font-size: 20px;
+    color: ${({ theme }) => theme.color.black};
+  }
+`;
+
+export const PeopleDetailsList = styled.ul`
+  padding: 0px;
+  list-style: none;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 24px;
+ 
+  @media (max-width: ${({ theme }) => theme.breakpoint.desktopMax}) {
+    margin: 16px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.desktop}) {
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 16px;
+    margin: 16px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.ipod}) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 16px;
+    margin: 16px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
+    display: flex;
+    flex-direction: column;
+    grid-gap: 16px;
+  }
+`;
+
+export const PeopleDetailsItemList = styled.li``;
