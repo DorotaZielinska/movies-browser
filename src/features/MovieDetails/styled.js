5627ebd0f7d1c87b3cc25e730fddc6dfeca6c5d6
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-21 17:22:55 +0200

    set linear-gradient and size of poster

--- src/features/MovieDetails/styled.js
+++ src/features/MovieDetails/styled.js
@@ -2,8 +2,10 @@ import styled from "styled-components";
 import { ReactComponent as starIcon } from "./images/star.svg";
 
 export const PosterContainer = styled.div`
-  width: 1928px;
-  height: auto;
+  width: 1368px;
+  height: 769px;
+  position: relative;
+  z-index: -1;
 `;
 
 export const Wrapper = styled.div`
@@ -14,6 +16,7 @@ export const Wrapper = styled.div`
   align-items: center;
   position: relative;
   overflow: hidden;
+  z-index: 1;
 `;
 
 export const PosterGradient = styled.div`
@@ -26,46 +29,48 @@ export const PosterGradient = styled.div`
   width: 100%;
   height: 100%;
   background: linear-gradient(
-      270deg,
+      91deg,
       rgb(0, 0, 0) 8%,
-      rgba(0, 0, 0, 0.87) 11.36%,
-      rgba(0, 0, 0, 0.72) 13.7%,
-      rgba(0, 0, 0, 0.29) 19.78%,
-      rgba(0, 0, 0, 0.16) 27.16%,
-      rgba(0, 0, 0, 0) 100%
+      rgba(0, 0, 0, 0.87) 10.36%,
+      rgba(0, 0, 0, 0.72) 12.7%,
+      rgba(0, 0, 0, 0.29) 20.78%,
+      rgba(0, 0, 0, 0.16) 23.16%,
+      rgba(0, 0, 0, 0) 28%
     ),
     linear-gradient(
-      91deg,
-      rgb(0, 0, 0) 15.58%,
-      rgba(0, 0, 0, 0.98) 4.88%,
-      rgba(0, 0, 0, 0.97) 9.31%,
-      rgba(0, 0, 0, 0) 24.78%,
-      rgba(0, 0, 0, 0.23) 23.83%,
-      rgba(0, 0, 0, 0) 24%
+      270deg,
+      rgb(0, 0, 0) 2.58%,
+      rgba(0, 0, 0, 0.98) 2.88%,
+      rgba(0, 0, 0, 0.97) 3.31%,
+      rgba(0, 0, 0, 0) 17.78%,
+      rgba(0, 0, 0, 0.23) 10.83%,
+      rgba(0, 0, 0, 0) 13%
     ),
     linear-gradient(
       rgb(0, 0, 0) 2%,
       rgba(0, 0, 0, 0.69) 4.23%,
-      rgba(0, 0, 0, 0.44) 9.57%,
-      rgba(0, 0, 0, 0.21) 15.6%,
+      rgba(0, 0, 0, 0.44) 12.57%,
+      rgba(0, 0, 0, 0.21) 19.6%,
       rgba(0, 0, 0, 0) 26%
     ),
     linear-gradient(
-      223deg,
-      rgba(0, 0, 0, 0) 74%,
-      rgba(0, 0, 0, 0.11) 76.64%,
-      rgba(0, 0, 0, 0.24) 79.16%,
-      rgba(0, 0, 0, 0.49) 86%,
-      rgba(0, 0, 0, 0.74) 89.61%,
-      rgb(0, 0, 0) 89%
+      203deg,
+      rgba(0, 0, 0, 0) 80%,
+      rgba(0, 0, 0, 0.11) 84.64%,
+      rgba(0, 0, 0, 0.24) 91.16%,
+      rgba(0, 0, 0, 0.49) 96%,
+      rgba(0, 0, 0, 0.74) 95.61%,
+      rgb(0, 0, 0) 100%
     );
 `;
 
 export const Poster = styled.img`
   z-index: 1;
   width: 100%;
-  height: 100%;
+  max-height: 769px;
   object-fit: cover;
+  position: relative;
+  z-index: -1;
 `;
 
 export const Title = styled.h1`
@@ -78,10 +83,9 @@ export const Title = styled.h1`
 export const MainInfo = styled.div`
   display: grid;
   grid-template-rows: repeat(2, auto);
-
   position: absolute;
-  top: 838px;
-  left: 276px;
+  bottom: 3%;
+  left: 5%;
 `;
 
 export const Rating = styled.div`
@@ -113,7 +117,7 @@ export const Slash = styled.span`
   font-size: 16px;
   font-weight: 400;
   line-height: 1.2;
-  align-self: last baseline;
+  align-self: center;
 `;
 export const Genre = styled.span`
   background-color: ${({ theme }) => theme.color.mystic};
