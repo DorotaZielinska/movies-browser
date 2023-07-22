41c29be8c7a7c1694b93c5fd6f50e34f8025865c
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-07-22 10:16:33 +0200

    added MovieTileListPeople for PeopleDetails index

--- src/common/TileList/TileMovie/index.js
+++ src/common/TileList/TileMovie/index.js
@@ -32,3 +32,27 @@ export const TileList = ({ title, year, genres, vote, votes, poster, id }) => {
     </GetDetailsLink>
   );
 };
+
+export const MovieTileListPeople = ({ title, year, genres, vote, votes, poster, id }) => {
+  const getYear = (string) => {
+    return string.split("-")[0];
+  };
+
+  return (
+    <GetDetailsLink to={`/people/${id}`} key={id}>
+      <MovieTileList>
+        <MovieImage poster={poster} />
+        <Content>
+          <div>
+            <StyledTitle>{title}</StyledTitle>
+            {year && <StyledSubTitle>{getYear(year)}</StyledSubTitle>}
+            <Genres genres={genres} />
+          </div>
+          <div>
+            <Rating vote={vote} votes={votes} />
+          </div>
+        </Content>
+      </MovieTileList>
+    </GetDetailsLink>
+  );
+};
