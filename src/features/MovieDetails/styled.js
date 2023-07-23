f813bf498303c9474032c231d44daa48d48729e8
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-23 17:57:36 +0200

    add styles for all devices

--- src/features/MovieDetails/styled.js
+++ src/features/MovieDetails/styled.js
@@ -2,16 +2,32 @@ import styled from "styled-components";
 import { ReactComponent as starIcon } from "./images/star.svg";
 
 export const PosterContainer = styled.div`
-  width: 1368px;
-  height: 769px;
-  position: relative;
-  z-index: -1;
+  max-width: 1368px;
+  max-height: 769px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.desktop}) {
+    max-width: 1200px;
+    max-height: 767px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.ipod}) {
+    max-width: 1024px;
+    max-height: 600px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
+    max-width: auto;
+    max-height: 400px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}) {
+    max-width: 450px;
+    max-height: 300px;
+  }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
     max-width: 320px;
     max-height: 148px;
-    display: flex;
-    flex-direction: column;
   }
 `;
 
@@ -36,22 +52,22 @@ export const PosterGradient = styled.div`
   width: 100%;
   height: 100%;
   background: linear-gradient(
-      91deg,
-      rgb(0, 0, 0) 8%,
-      rgba(0, 0, 0, 0.87) 10.36%,
-      rgba(0, 0, 0, 0.72) 12.7%,
-      rgba(0, 0, 0, 0.29) 20.78%,
-      rgba(0, 0, 0, 0.16) 23.16%,
-      rgba(0, 0, 0, 0) 28%
+      90deg,
+      rgb(0, 0, 0) 14%,
+      rgba(0, 0, 0, 0.87) 15.16%,
+      rgba(0, 0, 0, 0.72) 17.7%,
+      rgba(0, 0, 0, 0.29) 21.28%,
+      rgba(0, 0, 0, 0.16) 24.16%,
+      rgba(0, 0, 0, 0) 27%
     ),
     linear-gradient(
       270deg,
-      rgb(0, 0, 0) 2.58%,
-      rgba(0, 0, 0, 0.98) 2.88%,
-      rgba(0, 0, 0, 0.97) 3.31%,
-      rgba(0, 0, 0, 0) 17.78%,
-      rgba(0, 0, 0, 0.23) 10.83%,
-      rgba(0, 0, 0, 0) 13%
+      rgb(0, 0, 0) 12.58%,
+      rgba(0, 0, 0, 0.98) 13.88%,
+      rgba(0, 0, 0, 0.97) 15.31%,
+      rgba(0, 0, 0, 0) 19.78%,
+      rgba(0, 0, 0, 0.23) 17.83%,
+      rgba(0, 0, 0, 0) 18%
     ),
     linear-gradient(
       rgb(0, 0, 0) 2%,
@@ -69,20 +85,40 @@ export const PosterGradient = styled.div`
       rgba(0, 0, 0, 0.74) 95.61%,
       rgb(0, 0, 0) 100%
     );
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
-    max-width: 320px;
-    max-height: 148px;
-  }
 `;
 
 export const Poster = styled.img`
   z-index: 1;
-  width: 100%;
+  max-width: 1368px;
   max-height: 769px;
   object-fit: cover;
   position: relative;
   z-index: -1;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.desktopMax}) {
+    max-width: 1368px;
+    max-height: 900px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.desktop}) {
+    max-width: 1200px;
+    max-height: 767px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.ipod}) {
+    max-width: 900px;
+    max-height: 600px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
+    max-width: 500px;
+    max-height: 400px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}) {
+    max-width: 380px;
+    max-height: 300px;
+  }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
     max-width: 262px;
@@ -99,6 +135,23 @@ export const Title = styled.h1`
   margin: 0;
   color: ${({ theme }) => theme.color.white};
 
+  @media (max-width: ${({ theme }) => theme.breakpoint.desktopMax}) {
+    font-size: 56px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.desktop}) {
+    font-size: 48px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.ipod}) {
+    font-size: 34px;
+    margin-bottom: 8px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
+    font-size: 30px;
+  }
+
   @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
     font-size: 24px;
     font-weight: 600;
@@ -111,23 +164,31 @@ export const MainInfo = styled.div`
   display: grid;
   grid-template-rows: repeat(3 auto);
   position: absolute;
-  bottom: 3%;
-  left: 5%;
+  bottom: 56px;
+  left: 276;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.desktopMax}) {
+    left: 16px;
+    bottom: 16px;
+  }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
     display: inline;
-    left: 50px;
-    bottom: 100px;
-    position: relative;
+    bottom: 9px;
   }
 `;
 
 export const Rating = styled.div`
+  display: flex;
   gap: 8px;
   font-size: 30px;
   font-weight: 500;
   line-height: 1.3;
   color: ${({ theme }) => theme.color.white};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
+    font-size: 20px;
+  }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
     display: inline;
@@ -145,15 +206,25 @@ export const Votes = styled.span`
   line-height: 19.2px;
   color: ${({ theme }) => theme.color.white};
 
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
+    font-size: 13px;
+  }
+
   @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
     font-size: 10px;
     font-weight: 400;
     line-height: 1.2;
-    margin: 0 0 0 8px;
     max-width: 100px;
+    margin-left: 8px;
   }
 `;
+
 export const Star = styled(starIcon)`
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
+    width: 24px;
+    height: 24px;
+  }
+
   @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
     width: 16px;
     height: 16px;
@@ -169,6 +240,7 @@ export const Description = styled.span`
 `;
 
 export const Slash = styled.span`
+  margin-left: -1px;
   font-size: 16px;
   font-weight: 400;
   line-height: 1.2;
@@ -181,6 +253,7 @@ export const Slash = styled.span`
     margin-left: 2px;
   }
 `;
+
 export const Genre = styled.span`
   background-color: ${({ theme }) => theme.color.mystic};
   color: ${({ theme }) => theme.color.woodsmoke};
