175c31bfbbc92e88f6aa9ebffd38eff676e96d63
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-24 14:10:39 +0200

    add new props

--- src/common/TileList/TilePeople/index.js
+++ src/common/TileList/TilePeople/index.js
@@ -2,14 +2,14 @@ import { Container } from "../../Container";
 import { CharacterName, PersonName, Wrapper, Content } from "./styled";
 import { PersonImage } from "./Image";
 
-export const PeopleListTile = ({ name, poster }) => {
+export const PeopleListTile = ({ name, poster, character }) => {
   return (
     <Container>
       <Wrapper>
         <PersonImage poster={poster} />
         <Content>
           <PersonName>{name}</PersonName>
-          <CharacterName></CharacterName>
+          <CharacterName>{character}</CharacterName>
         </Content>
       </Wrapper>
     </Container>
