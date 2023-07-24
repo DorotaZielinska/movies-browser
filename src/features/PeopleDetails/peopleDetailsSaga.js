95ec46b479b0be9f03085d4439cdbf7968ddd0a7
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-07-24 08:54:06 +0200

    cleaning in peopleDetailsSaga remove  consolelog..

--- src/features/PeopleDetails/peopleDetailsSaga.js
+++ src/features/PeopleDetails/peopleDetailsSaga.js
@@ -2,13 +2,13 @@ import { select, put, delay, takeLatest, call } from "redux-saga/effects";
 import { fetchPeopleDetailsError, fetchPeopleDetailsSuccess, getPersonId, selectPersonId } from "./peopleDetailsSlice";
 import { getPeopleDetails, getPeopleMovieCredits } from "../getDataApi";
 
-function* fetchPeopleDetailsHandler() {
+function* fetchPeopleDetailsHandler({}) {
     try{
         yield delay(300);
         const id = yield select(selectPersonId);
-        const details = yield call(getPeopleDetails, {personId: id});
-        const credits = yield call(getPeopleMovieCredits, {personId: id});
-        yield put(fetchPeopleDetailsSuccess({ credits, details}));
+        const details = yield call(getPeopleDetails, id);
+        const credits = yield call(getPeopleMovieCredits, id);
+        yield put(fetchPeopleDetailsSuccess({ credits, details,}));
     } catch (error) {
         yield put(fetchPeopleDetailsError());
     }
