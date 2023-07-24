19d6625a48033894ad169e31d12e6050287d163a
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-07-24 08:54:55 +0200

    cleaning, removed some part which dosent have use

--- src/features/PeopleDetails/index.js
+++ src/features/PeopleDetails/index.js
@@ -1,36 +1,27 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Container } from "../../common/Container";
 import { ActorDescriptionTile } from "../../common/TileList/PeopleDetailsTile";
-import { MovieTileListPeople } from "../../common/TileList/TileMovie";
-import {
-  ItemPeopleDetails,
-  ListPeopleDetails,
-  TitlePeopleDetails,
-} from "./styled";
-import { useDispatch, useSelector } from "react-redux";
+import { SubtitleBirth } from "./styled";
 import {
   getPersonId,
   selectDetailsStatus,
-  selectPeopleCast,
-  selectPeopleCrew,
   selectPeopleDetails,
 } from "./peopleDetailsSlice";
 import { Loading } from "../AsideActions/Loading/loading";
 import { Error } from "../AsideActions/Error/error";
-import { useEffect } from "react";
 
 export const PeopleDetails = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
+  const status = useSelector(selectDetailsStatus);
+  const details = useSelector(selectPeopleDetails);
+  const screenWidth = window.innerWidth;
 
   useEffect(() => {
     dispatch(getPersonId({ personId: id }));
   }, [id, dispatch]);
-
-  const status = useSelector(selectDetailsStatus);
-  const cast = useSelector(selectPeopleCast);
-  const details = useSelector(selectPeopleDetails);
-  const crew = useSelector(selectPeopleCrew);
 
   return status === "error" ? (
     <Error />
@@ -41,53 +32,22 @@ export const PeopleDetails = () => {
       <ActorDescriptionTile
         poster={details.profile_path}
         title={details.name}
-        dateOfBirth={details.birthday}
-        placeOfBirth={details.place_of_birth}
+        dateOfBirth={
+          <>
+            <SubtitleBirth>
+              {screenWidth < 450 ? "Birth: " : "Date of Birth: "}
+            </SubtitleBirth>
+            {details.birthday.split("-").reverse().join(".")}
+          </>
+        }
+        placeOfBirth={
+          <>
+            <SubtitleBirth>Place of birth: </SubtitleBirth>
+            {details.place_of_birth}
+          </>
+        }
         overview={details.biography}
       />
-      {cast.length > 0 && (
-        <>
-          <TitlePeopleDetails>{`Movies - cast (${cast.filter(movie => movie.title).length})`}</TitlePeopleDetails>
-          <ListPeopleDetails>
-            {cast.map((movie) => movie.title && (
-              <ItemPeopleDetails key={movie.credit_id}>
-                <MovieTileListPeople
-                  id={movie.id}
-                  poster={movie.poster_path}
-                  title={movie.title}
-                  character={movie.character}
-                  year={movie.release_date}
-                  vote={movie.vote_average}
-                  votes={movie.vote_count}
-                  genres={movie.genre_ids}
-                />
-              </ItemPeopleDetails>
-            ))}
-          </ListPeopleDetails>
-        </>
-      )}
-      ;
-      {crew.length > 0 && (
-        <>
-          <TitlePeopleDetails>{`Movies - crew (${crew.filter(movie => movie.title).length})`}</TitlePeopleDetails>
-          <ListPeopleDetails>
-            {crew.map((movie) => (
-              <ItemPeopleDetails key={movie.credit_id}>
-                <MovieTileListPeople
-                  id={movie.id}
-                  poster={movie.poster_path}
-                  title={movie.title}
-                  character={movie.character}
-                  year={movie.release_date}
-                  vote={movie.vote_average}
-                  votes={movie.vote_count}
-                  genres={movie.genre_ids}
-                />
-              </ItemPeopleDetails>
-            ))}
-          </ListPeopleDetails>
-        </>
-      )}
     </Container>
   );
 };
