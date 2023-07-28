1739c5d745c7062f2a8cba2193d9f05144fd7b82
Pawel Owiesek <Owiesek.p@gmail.com>
2023-07-28 13:52:18 +0200

    add BackgroundSpace component

--- src/features/MovieDetails/index.js
+++ src/features/MovieDetails/index.js
@@ -32,14 +32,13 @@ import {
   TotalVotes,
   Vote,
   CountryShort,
+  BackgroundSpace,
 } from "./styled";
 import { MovieDetailsTile } from "../../common/TileList/MovieDetailsTile";
 import { PeopleListTile } from "../../common/TileList/TilePeople";
-import { useState } from "react";
 const image = "https://image.tmdb.org/t/p/original/";
 
 export const MovieDetails = () => {
-  const [isLoading, setLoading] = useState(true);
   const { id } = useParams();
   const dispatch = useDispatch();
   const status = useSelector(selectMovieState);
@@ -56,16 +55,6 @@ export const MovieDetails = () => {
     };
   }, [dispatch]);
 
-  useEffect(() => {
-    const loadingTimeout = setTimeout(() => {
-      setLoading(false);
-    }, 400);
-
-    return () => {
-      clearTimeout(loadingTimeout);
-    };
-  }, []);
-
   return status === "loading" || !details ? (
     <Loading />
   ) : status === "error" ? (
@@ -74,98 +63,99 @@ export const MovieDetails = () => {
     <>
       <Wrapper>
         {details.backdrop_path === null ? null : (
-          <PosterContainer>
-            <Poster src={`${image}${details.backdrop_path}`} alt="poster" />
-            <PosterGradient />{" "}
-            <MainInfo>
-              <Title>{details.title}</Title>
-              <Rating>
-                <Star />
-                {details.vote_average.toFixed(1)}
-                <Slash>/ 10</Slash>
-              </Rating>
-              <Votes>{details.vote_count} votes</Votes>
-            </MainInfo>
-          </PosterContainer>
+          <BackgroundSpace>
+            <PosterContainer>
+              <Poster src={`${image}${details.backdrop_path}`} alt="poster" />
+              <PosterGradient />{" "}
+              <MainInfo>
+                <Title>{details.title}</Title>
+                <Rating>
+                  <Star />
+                  {details.vote_average.toFixed(1)}
+                  <Slash>/ 10</Slash>
+                </Rating>
+                <Votes>{details.vote_count} votes</Votes>
+              </MainInfo>
+            </PosterContainer>
+          </BackgroundSpace>
         )}
       </Wrapper>
-      {isLoading ? null : (
-        <>
-          <Container>
-            <MovieDetailsTile
-              content={details.overview}
-              poster={details.poster_path}
-              title={details.title}
-              year={details.release_date}
-              place={
-                <>
-                  <Description>Production: </Description>
-                  <Country>
-                    {details.production_countries
-                      .map((country) => country.name)
-                      .join(", ")}
-                  </Country>
-                  <CountryShort>
-                    {details.production_countries
-                      .map((country) => country.iso_3166_1)
-                      .join(", ")}
-                  </CountryShort>
-                </>
-              }
-              date={
-                <>
-                  <Description>Release date: </Description>
-                  <Date>
-                    {" "}
-                    {details.release_date.split("-").reverse().join(".")}
-                  </Date>
-                </>
-              }
-              vote={
-                <>
-                  <Vote> {details.vote_average.toFixed(1)}</Vote>
-                  <Slash>/ 10</Slash>
-                </>
-              }
-              votes={
-                <>
+
+      <>
+        <Container>
+          <MovieDetailsTile
+            content={details.overview}
+            poster={details.poster_path}
+            title={details.title}
+            year={details.release_date}
+            place={
+              <>
+                <Description>Production: </Description>
+                <Country>
+                  {details.production_countries
+                    .map((country) => country.name)
+                    .join(", ")}
+                </Country>
+                <CountryShort>
+                  {details.production_countries
+                    .map((country) => country.iso_3166_1)
+                    .join(", ")}
+                </CountryShort>
+              </>
+            }
+            date={
+              <>
+                <Description>Release date: </Description>
+                <Date>
                   {" "}
-                  <TotalVotes>{details.vote_count} votes </TotalVotes>
-                </>
-              }
-              genres={details.genres.map((genre) => (
-                <Genre key={genre.id}>{genre.name}</Genre>
-              ))}
-            />
-          </Container>
-          <Container>
-            <SectionTitle>Cast</SectionTitle>
-            <CastList>
-              {credits.cast.map((person) => (
-                <PeopleListTile
-                  id={person.id}
-                  name={person.name}
-                  poster={person.profile_path}
-                  character={person.character}
-                />
-              ))}
-            </CastList>
-          </Container>
-          <Container>
-            <SectionTitle>Crew</SectionTitle>
-            <CrewList>
-              {credits.crew.map((person) => (
-                <PeopleListTile
-                  id={person.id}
-                  name={person.name}
-                  poster={person.profile_path}
-                  character={person.job}
-                />
-              ))}
-            </CrewList>
-          </Container>
-        </>
-      )}
+                  {details.release_date.split("-").reverse().join(".")}
+                </Date>
+              </>
+            }
+            vote={
+              <>
+                <Vote> {details.vote_average.toFixed(1)}</Vote>
+                <Slash>/ 10</Slash>
+              </>
+            }
+            votes={
+              <>
+                {" "}
+                <TotalVotes>{details.vote_count} votes </TotalVotes>
+              </>
+            }
+            genres={details.genres.map((genre) => (
+              <Genre key={genre.id}>{genre.name}</Genre>
+            ))}
+          />
+        </Container>
+        <Container>
+          <SectionTitle>Cast</SectionTitle>
+          <CastList>
+            {credits.cast.map((person) => (
+              <PeopleListTile
+                id={person.id}
+                name={person.name}
+                poster={person.profile_path}
+                character={person.character}
+              />
+            ))}
+          </CastList>
+        </Container>
+        <Container>
+          <SectionTitle>Crew</SectionTitle>
+          <CrewList>
+            {credits.crew.map((person) => (
+              <PeopleListTile
+                id={person.id}
+                name={person.name}
+                poster={person.profile_path}
+                character={person.job}
+              />
+            ))}
+          </CrewList>
+        </Container>
+      </>
     </>
   );
 };
