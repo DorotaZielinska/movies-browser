f5a21f5b165f80371e7bbb883214b7861f9116cd
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-07-29 17:44:39 +0200

    added getGenres from Api

--- src/features/PeopleDetails/peopleDetailsSaga.js
+++ src/features/PeopleDetails/peopleDetailsSaga.js
@@ -1,20 +1,20 @@
 import { select, put, delay, takeLatest, call } from "redux-saga/effects";
 import {
   fetchPeopleDetailsError,
-  fetchPeopleDetailsLoad,
   fetchPeopleDetailsSuccess,
   getPersonId,
   selectPersonId,
 } from "./peopleDetailsSlice";
-import { getPeopleDetails, getPeopleMovieCredits } from "../getDataApi";
+import { getGenres, getPeopleDetails, getPeopleMovieCredits } from "../getDataApi";
 
 function* fetchPeopleDetailsHandler() {
   try {
     yield delay(300);
     const id = yield select(selectPersonId);
+    const genres = yield call(getGenres)
     const details = yield call(getPeopleDetails, id);
     const credits = yield call (getPeopleMovieCredits, id)
-    yield put(fetchPeopleDetailsSuccess({ details,credits }));
+    yield put(fetchPeopleDetailsSuccess({ details,credits, genres }));
   } catch (error) {
     yield put(fetchPeopleDetailsError());
   }
