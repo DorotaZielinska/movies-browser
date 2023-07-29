0592d4a11aeed47b2d654800c50cdeb74a6bbc6e
DorotaZielinska <122475780+DorotaZielinska@users.noreply.github.com>
2023-07-29 17:48:40 +0200

    added genresIdDetails

--- src/common/Genres/index.js
+++ src/common/Genres/index.js
@@ -1,21 +1,28 @@
 import { useSelector } from "react-redux";
 import { GenresContainer, Genre } from "./styled";
 import { selectGenres } from "../../features/MovieList/movieListSlice";
+import { selectGenresDetails } from "../../features/PeopleDetails/peopleDetailsSlice";
 
 export const Genres = ({ genres }) => {
-    const genresId = useSelector(selectGenres)
+  const genresId = useSelector(selectGenres);
+  const genresIdDetails = useSelector(selectGenresDetails);
+  const selectGenresId = genresId !== undefined ? genresId : genresIdDetails;
+  const getGenresId = (ids, id) => {
+    if (id) {
+      return ids
+        .map((ids) => id.filter(({ id }) => id === ids)[0]?.name)
+        .filter(Boolean);
+    }
+    return [];
+  };
 
-    const getGenresId = (ids, id) => {
-        return ids.map((ids) => id.filter(({ id }) => id === ids)[0].name);
-    };
-
-    return (
-       genres && (
-        <GenresContainer>
-            {getGenresId(genres, genresId).map((genre) => (
-                <Genre key={genre}>{genre}</Genre>
-            ))}
-        </GenresContainer>
-       )
-    );
+  return (
+    genres && (
+      <GenresContainer>
+        {getGenresId(genres, selectGenresId).map((genre) => (
+          <Genre key={genre}>{genre}</Genre>
+        ))}
+      </GenresContainer>
+    )
+  );
 };
